f79867ae52551c60a87556d20b1aa7e61b270628
Thomas Grammatico <thomas@grammatico.me>
2025-10-18 12:41:24 +0200

    Added quartiles to mart monthly job cat

--- src/dbt/models/market/mart_monthly_by_job_cat.sql
+++ src/dbt/models/market/mart_monthly_by_job_cat.sql
@@ -6,7 +6,7 @@ with source as (
 select
 
     dim_job_category,
-     date_trunc('month', dim_published_at) as month,
+    date_trunc('month', dim_published_at) as month,
 
     SUM(CASE WHEN dim_experience LIKE '%< 2%' THEN 1 ELSE 0 END)::FLOAT / COUNT(*) * 100 as pct_junior,
     SUM(CASE WHEN dim_experience LIKE '%2 à 5%' THEN 1 ELSE 0 END)::FLOAT / COUNT(*) * 100 as pct_confirme,
@@ -25,17 +25,18 @@ select
 
     avg(fact_salary_min) as mean_salary_min,
     avg(fact_salary_max) as mean_salary_max,
-    min(fact_salary_min) as min_salary,
-    max(fact_salary_max) as max_salary,
+    quantile_cont(fact_salary_min, [0.0, 0.25, 0.5, 0.75, 1.0]) as salary_min_quantiles,
+    quantile_cont(fact_salary_max, [0.0, 0.25, 0.5, 0.75, 1.0]) as salary_max_quantiles,
 
     avg(fact_daily_rate_min) as mean_daily_rate_min,
     avg(fact_daily_rate_max) as mean_daily_rate_max,
-    min(fact_daily_rate_min) as min_daily_rate,
-    max(fact_daily_rate_max) as max_daily_rate,
+    quantile_cont(fact_daily_rate_max, [0.0, 0.25, 0.5, 0.75, 1.0]) as tjm_max_quantiles,
+    quantile_cont(fact_daily_rate_min, [0.0, 0.25, 0.5, 0.75, 1.0]) as tjm_min_quantiles,
+
+
 
     avg(fact_duration) as mean_duration,
-    min(fact_duration) as min_duration,
-    max(fact_duration) as max_duration,
+    quantile_cont(fact_duration, [0.0, 0.25, 0.5, 0.75, 1.0]) as duration_quantiles,
 
 from source
 group by dim_job_category, date_trunc('month', dim_published_at)
