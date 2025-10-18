cf9b9e2c9d598c7433f757de091c3c0ab12f81b0
Thomas Grammatico <thomas@grammatico.me>
2025-10-18 10:32:57 +0200

    Remove dedupe cte in mart job listings. Fixed typo that duplicated columns.

--- src/dbt/models/market/mart_job_listings.sql
+++ src/dbt/models/market/mart_job_listings.sql
@@ -2,15 +2,6 @@ with source as (
     select *
     from {{ref("int_job_listings_one_hot_contract_type")}}
     where dbt_valid_to = CAST('9999-12-31' AS DATE)
-),
-
-deduped as (
-    select *,
-        row_number() over (
-            partition by job_url
-            order by dbt_updated_at desc, updated_at desc
-        ) as rn
-    from source
 )
 
 select
@@ -27,12 +18,12 @@ select
     start_date as dim_start_date,
 
     is_open_to_freelance as dim_is_open_to_freelance,
-    is_open_to_cdi as dim_is_open_to_freelance,
-    is_open_to_cdd as dim_is_open_to_freelance,
+    is_open_to_cdi as dim_is_open_to_cdi,
+    is_open_to_cdd as dim_is_open_to_cdd,
 
-    is_fully_remote as dim_is_open_to_freelance,
-    is_partially_remote as dim_is_open_to_freelance,
-    is_not_remote as dim_is_open_to_freelance,
+    is_fully_remote as dim_is_fully_remote,
+    is_partially_remote as dim_is_partially_remote,
+    is_not_remote as dim_is_not_remote,
 
     ASAP_start as dim_ASAP_start,
 
@@ -46,5 +37,4 @@ select
     -- Audit fields
     updated_at
 
-from deduped
-where rn = 1
+from source
